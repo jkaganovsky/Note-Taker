4aab9f84bad3c2634bd83827aa466c69916adb3c
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-12-15 18:48:05 -0800

    Changed PORT to work on heroku.

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
-const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +17,17 @@ app.get("/", function(req, res) {
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+});
+
+// Posting new Note
+app.post("/notes", function(req,res) {
+    var newNote = req.body;
+
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    res.json(newNote);
 });
 
 // Start server
