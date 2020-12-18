66f8ff0d62fd457ef5e24e9257452900ab4cfec2
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-12-17 21:51:26 -0800

    Wrote get and post api codes that saves and pushes new notes to the page.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { json } = require("express");
+
+const notesList = [];
 
 const PORT = 3000;
 // const PORT = process.env.PORT || 3001;
@@ -12,6 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
+app.get("/api/notes", function(req, res) {
+    fs.readFile(__dirname + "/db/db.json", function(err, jsonString) {
+        if (err) {
+            console.log("File read failed:", err);
+            return;
+        }
+        else {
+            const notes = JSON.parse(jsonString)
+            console.log("db.json file:", notes);
+        }
+    });
+    res.json(notesList);
+    console.log("What am I:", notesList);
+});
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
 });
@@ -20,24 +38,13 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-});
-
-const notes = [
-    {
-        title: "Test Title",
-        text: "Test text"
-    }
-]
-
 // Posting new Note
 app.post("/api/notes", function(req,res) {
     const newNote = req.body;
 
-    console.log(newNote);
+    console.log("New note log:", newNote);
 
-    notes.push(newNote);
+    notesList.push(newNote);
 
     res.json(newNote);
 });
