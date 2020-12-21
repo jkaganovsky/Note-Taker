fc586075653ff1c3e846aab438bb73b89c7a8fd8
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2020-12-21 01:32:34 -0800

    Deleted duplicate and unnecessary codes, wrote the app.delete code, fixed index code on app.post.

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const notesList = [];
-
-const OUTPUT_DIR = path.resolve(__dirname, "db");
-const outputPath = path.join(OUTPUT_DIR, "db.json");
-
-// const PORT = 3000;
-const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 // Routes
 app.get("/api/notes", function(req, res) {
     const fileData = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
 
-    console.log("Notes Read File:", fileData);
+    console.log("Read file:", fileData);
 
     res.json(fileData);
 });
@@ -28,30 +22,11 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
 });
 
-app.get("/api/notes/:id", function(req, res) {
-    const selectNote = req.params.id;
-    console.log(selectNote);
-
-    return res.send(req.params.id);
-
-    // const fileData = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
-
-    // console.log("Notes:", fileData);
-
-    // for (let i = 0; i < fileData.length; i++) {
-    //   if (selectNote === fileData[i].routeName) {
-    //     return res.send(fileData[i]);
-    //   }
-    // }
-
-    // return res.send("No notes found");
-  });
-
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
 });
 
-// Posting new Note
+// Posting new note
 app.post("/api/notes", function(req,res) {
     const newNote = req.body;
 
@@ -62,9 +37,18 @@ app.post("/api/notes", function(req,res) {
     const fileData = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
     console.log("Before push:", fileData);
 
-    const lastIndex = (fileData.length - 1);
+    // Adds an id string to an array
+    console.log("lastIndex:", fileData.length - 1);
+    console.log("newIndex:", fileData.length + 2);
+    if (fileData.length === 0) {
+        console.log("empty fileData");
+        newNote.id = 1;
+    } else {
+        const lastIndex = (fileData.length - 1);
 
-    newNote.id = fileData[lastIndex].id + 1;
+        console.log("Not empty fileData");
+        newNote.id = fileData[lastIndex].id + 1;
+    }
 
     fileData.push(newNote);
     console.log("After push:", fileData);
@@ -72,19 +56,24 @@ app.post("/api/notes", function(req,res) {
     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(fileData));
 });
 
-// Deleting Notes
+// Deleting notes by id
 app.delete("/api/notes/:id", function(req, res) {
-    const deleteNote = req.params.id;
+    const deleteNote = parseInt(req.params.id);
+    console.log("Delete note:", typeof deleteNote);
 
-    console.log(deleteNote);
+    const fileData = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
+    console.log("Read data:", fileData);
 
-    for (let i = 0; i < notesList.length; i++) {
-        if (deleteNote === notesList[i].routeName) {
-            return res.send(notesList[i]);
+    //
+    for (let i = 0; i < fileData.length; i++) {
+        console.log("For loop:", fileData[i]);
+        if (deleteNote === fileData[i].id) {
+            fileData.splice(i, 1);
+            console.log("Splice data:", fileData);
         }
     }
 
-    return res.send(false);
+    return res.send(fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(fileData)));
 });
 
 // Start server
